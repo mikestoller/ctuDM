898a5a6e992a63f6a9985627d9378e3869bd1cf5
mikestoller <mstoller84@gmail.com>
2018-03-28 22:57:56 +0200

    on and on

--- R/plus_collapse.R
+++ R/plus_collapse.R
@@ -1,9 +1,9 @@
 
 #' @name plus_collapse
 #' @export plus_collapse
-#' @title Collapses input (as rows) from clipboard with plus sign and adds square brackets.
+#' @title Collapses input (as rows) from clipboard with plus sign and adds optional square brackets.
 #'
-#' @description This function gets rows from the clipboard and outputs the rows (with square brackets) connected by plus signs.
+#' @description This function gets rows from the clipboard and outputs the rows (with optional square brackets) connected by plus signs.
 #'
 #' @param brackets TRUE by default. If TRUE, inserts square brackets around each row. If FALSE, does not insert brackets.
 
