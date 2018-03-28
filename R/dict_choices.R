6b2769061c60ca5fe75d1faa3ccefe8664f3342b
mikestoller <mstoller84@gmail.com>
2018-03-28 22:05:13 +0200

    add functions

---
+++ R/dict_choices.R
@@ -0,0 +1,36 @@
+#' @name dict_choices
+#' @export dict_choices
+#' @title Creates choices from rows.
+#'
+#' @description This function gets variables (in rows) from the clipboard and outputs choices for pasting into the REDCap dictionary.
+#'
+#' @param stepsize 1 by default. The stepsize by which to increment the choice value from one choice to the next.
+#' @param stepmin 1 by default. The value for the first choice
+#'
+#' @return A one-cell string with formatting according to choices in REDCap dictionary.
+#'
+#' @author Michael Stoller <mstoller84@gmail.com> <michael.stoller@ctu.unibe.ch>
+#'
+#' @examples
+#' \dontrun{
+#' #Run function with stepsize 1 and initial step 0
+#' dict_choices(stepsize = 1, stepmin = 0)
+#' }
+#' @importFrom utils readClipboard
+
+dict_choices <- function(stepsize=1,stepmin=1){
+
+  x <- readClipboard() # get clipboard input
+
+  steps = seq(from=stepmin,by=stepsize,length.out=length(x))
+
+  for(i in 1:length(x)){
+    x[i] <- paste0(steps[i],", ",x[i])
+  }
+
+  # put together
+
+  y <- paste(x[1:length(x)],collapse=" | ")
+
+  writeClipboard(y)
+}
---
+++ R/rc_batch.R
@@ -0,0 +1,145 @@
+# -------------------------------------------------------------------------
+# This is a R script
+# -------------------------------------------------------------------------
+# File          : rc_batch.R
+# Author        : Michael Stoller, mstoller84@gmail.com
+# Date Created  : 2018-01-16
+# Last Change   : 2018-
+# Institute     : CTU
+# Topic         : Statistics
+# -------------------------------------------------------------------------
+# Description:
+# - functions to automate choices, branching logic, etc.
+#
+# changelog:
+#
+# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------
+
+
+event_br <- function(){
+
+  x <- readClipboard() # get clipboard input
+
+  for(i in 1:length(x)){
+    x[i] <- paste0("event_id=",x[i])
+  }
+
+  # put together
+
+  y <- paste(x[1:length(x)],collapse=" OR ")
+
+  writeClipboard(y)
+
+}
+
+
+warnbatch <- function(repeatn = 1, multiling = 0){
+
+
+  x <- readClipboard() # get variables from clipboard
+  len <- length(x) # number of variables
+
+  # counter for questions
+  c <- rep(seq(1,len/3),1,each=repeatn)
+
+  # counter for language
+  l <- rep(seq(1,repeatn),len/3,each=1)
+
+  # preallocate
+  y <- vector("list",0)
+
+  #combinations
+  for(i in 1:len){
+    y$label[i] = paste0("<div class='red' style='text-align:center;'><h4>Please complete question #",c[i],'</h4></div>')
+    y$varname[i] = paste0("wrn_",x[i],"_txt")
+    if (multiling){
+      y$brlog[i] = paste0("[",x[i],"]='' AND [language_radio]=",l[i])
+    }else{
+      y$brlog[i] = paste0("[",x[i],"]=''")
+    }
+  }
+
+  #varname output
+  copy.table(y)
+
+}
+
+quickor <- function(){
+
+  x <- readClipboard()
+
+  y <- paste0(" ",x," ",collapse=" OR ")
+
+  writeClipboard(y)
+
+}
+
+quickplus <- function(){
+
+  x <- readClipboard()
+
+  y <- paste0(" ",x," ",collapse=" + ")
+
+  writeClipboard(y)
+
+}
+
+quickcomma <- function(brackets=FALSE){
+
+  x <- readClipboard()
+
+  if (brackets){
+
+    y <- paste0("[",x,"]",collapse=",")
+
+
+  } else{
+  y <- paste0(" ",x," ",collapse=",")
+  }
+
+  writeClipboard(y)
+
+}
+
+extract_from_quotes <- function(){
+  x <- readClipboard()
+
+  y <- str_extract_all(x,"\".*?\"",simplify = TRUE)
+
+  y <- str_sub(y,start=2,end=-2)
+
+
+}
+
+number_of_nonempty_records <- function(radio=FALSE){
+
+  x <- readClipboard()
+
+  y <- vector()
+
+  if (radio){
+
+  for (i in 1:length(x)){
+
+    y[i] = paste0('if( [',x[i],'] = "NaN",0,1)')
+
+  }
+
+  } else {
+
+    for (i in 1:length(x)){
+
+      y[i] = paste0('if( [',x[i],'] = "",1,0)')
+
+    }
+
+  }
+
+
+  y <- paste0(y,collapse = ",")
+  y <- paste0('sum(',y,')')
+
+  writeClipboard(y)
+
+}
